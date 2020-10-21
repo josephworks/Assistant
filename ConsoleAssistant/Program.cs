ae7cdbbf0392a25c6b1c9745ed1edec5343c5db2
Joseph Robinson <smartech56@gmail.com>
2020-10-20 18:09:05 -0700

    General Updates

---
+++ ConsoleAssistant/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.CognitiveServices.Speech;
+using SpeechLib.Synthesis;
+using static System.Console;
+
+namespace ConsoleAssistant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String msg = "Cystemz is really bad";
+            Console.WriteLine(msg);
+
+            SpeechLib.Synthesis.SpeechSynthesis speechSynthesis = new SpeechSynthesis();
+            speechSynthesis.Speak(msg);
+
+            Console.ReadLine();
+        }
+    }
+}
